4e6c8a76a48ef1dbf26b187ed2d4de5847a45227
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2025-09-08 01:22:29 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,85 @@
+a = 34
+b = 2
+
+# Operators in Python :-
+
+# Types of Operators
+
+# Arithmetic Operators:
+
+# + (Addition), - (Subtraction), * (Multiplication), / (Division), % (Modulus), ** (Exponentiation), // (Floor Division).
+
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+print("a % b = ", a % b)
+print("a // b = ", a // b)
+print("a ** b = ", a ** b)
+
+
+# Comparison Operators:
+
+# == (Equal), != (Not Equal), > (Greater Than), < (Less Than), >= (Greater Than or Equal), <= (Less Than or Equal).
+
+print(a > 4)
+print(a < 4)
+print(a <= 4)
+print(a >= 4)
+print(a == 4) # Is a equal to 4?
+print(a == 34) # Is a equal to 34?
+print(a != 34) # Is a not equal to 34?
+
+# Logical Operators: and, or, not.
+
+c = True
+d = False
+
+print("\nFor AND Operator: .....")
+print(True and False)
+print(False and False)
+print(True and True)
+print(False and True)
+
+print("\nFor OR Operator: ......")
+print(True or False)
+print(False or False)
+print(True or True)
+print(False or True)
+
+print("\nFor NOT Operator: .....")
+print(not(True))
+print(not(False))
+
+# Assignment Operator:
+e = 24
+f = 20
+g = 28
+
+print(e)
+e += 4    # Equivalent to e = e + 4
+print(e)
+
+print(f)
+f -= 4    # Equivalent to f = f - 4
+print(f)
+
+print(g)
+g *= 4    # Equivalent to g = g x 4
+print(g)
+
+x = 10
+print(x)
+x %= 5    # Equivalent to x = x % 5
+print(x) 
+
+y = 42
+print(y)
+y **= 5   # Equivalent to y = y ** 5
+print(y)
+
+z = 35
+print(z)
+z //= 4   # Equivalent to z = z // 4
+print(z)
+
