b6f8595be7b9c47ba568878b6f2f6f1aa12baa2f
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2025-09-09 01:31:54 +0530

    Add files via upload

---
+++ Infinite_loops.py
@@ -0,0 +1,13 @@
+# Infinite Loops:
+
+# Be careful to avoid infinite loops by ensuring the condition eventually becomes False.
+# Example of an infinite loop:
+
+while True:
+    print("This will run forever!")
+
+
+i = 1
+while True:
+    print(i)
+    i + 1
---
+++ While_loops.py
@@ -0,0 +1,19 @@
+# What are While Loops?
+
+# While loops execute a block of code as long as a condition is True.
+# They are useful when the number of iterations is not known in advance.
+
+i = 1
+while i<6:
+    print (i)
+    i = i+1
+
+print("\n")
+
+# Example :-
+
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1
