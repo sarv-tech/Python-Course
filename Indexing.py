bf5fcfd66404c1ad45bc658dd3d69c75b2beab4a
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2025-09-09 20:54:16 +0530

    Add files via upload

---
+++ Indexing.py
@@ -0,0 +1,11 @@
+name = "Sarvesh"
+
+# name = "S    A    R    V    E    S    H"
+#         0    1    2    3    4    5    6
+#        -7   -6   -5   -4   -3   -2   -1
+
+print(name[0],name[1],name[2],name[3],name[4],name[5],name[6]) # Positive Indexes
+
+print("\n")
+
+print(name[-7],name[-6],name[-5],name[-4],name[-3],name[-2],name[-1]) # Negative Indexes
---
+++ strings.py
@@ -0,0 +1,31 @@
+# Strings are one of the most fundamental data types in Python. A string is a sequence of characters enclosed within either single quotes ('), double quotes ("), or triple quotes (''' or """).
+
+# Creating Strings
+# You can create strings in Python using different types of quotes:
+
+
+# name = "Sarvesh"
+name = '''Sarvesh is
+a good
+boy'''
+# name = """Sarvesh is a good boy"""
+print(name)
+
+print("\n")
+
+# Example:-
+
+# Single-quoted string
+a = 'Hello, Python!'
+
+# Double-quoted string
+b = "Hello, World!"
+
+# Triple-quoted string (useful for multi-line strings)
+c = '''This is
+a multi-line
+string.'''
+
+print(a)
+print(b)
+print(c)
