022a026b868e96e69808636e850172139ad00152
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2025-09-13 23:58:58 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,9 @@
+a = (3, 22, 2, 13)
+
+print(a)
+print(a[2])
+a[3] = 4         # Run-time error will be seen because tuples is immutable and its can't   be    changed after creation
+
+my_tuple = (10, 20, 30)
+single_element = (5,)  # Tuple with one element (comma required)
+print(my_tuple[1])
