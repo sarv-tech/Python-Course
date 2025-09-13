932745c8422fc9ba3f70ed384289f7b857147457
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2025-09-13 21:55:22 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,10 @@
+# Lists are ordered, mutable (changeable) collections of items.
+
+marks = [28, 42, 9.2, 101, 23]
+mixed  = [1, "Hello", False, 3.14]
+
+print(marks[2:4])
+print(marks[5])     # It throws run time error because it's index is out of bound
+print(marks[0])
+print(mixed[1])
+print(mixed[4])     # Error Index is out of bound
