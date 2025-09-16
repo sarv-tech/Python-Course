591df29b12ac6769b38c2b17cf3ecca8c349fbbf
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2025-09-17 00:44:02 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,8 @@
+# Dictionaries store key-value pairs and allow fast lookups.
+
+marks = {"Sarvesh": 75, "Ravi": 85, "Anita": 90}
+
+print(marks, type(marks))  # Printing the dictionary and its type
+print(marks["Sarvesh"])  # Accessing value by key
+marks["Ravi"] = 54  # Updating value
+print(marks)
