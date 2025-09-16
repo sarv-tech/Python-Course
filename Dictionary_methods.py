40b6056914165c1464f87a14dd686d59ff9d3212
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2025-09-17 00:56:59 +0530

    Add files via upload

---
+++ Dictionary_methods.py
@@ -0,0 +1,13 @@
+marks = {"Sarvesh": 75, "Ravi": 85, "Anita": 90}
+
+print(marks.keys())   
+
+print(marks.values()) 
+
+marks.pop("Ravi")
+print(marks)
+
+print(marks.items())
+
+marks.clear()
+print(marks)
