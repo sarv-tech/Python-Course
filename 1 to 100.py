a8f19f27086306011886ff31bfa773d64ef4708a
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2025-12-15 01:27:06 +0530

    Add files via upload

---
+++ 1 to 100.py
@@ -0,0 +1,6 @@
+# Write a program to print all numbers from 1 to 100 that are divisible by both 3 and 5.
+
+for i in range (1, 101):
+    if(i % 3.0 == 0 and i % 5.0 == 0):
+        print(i)
+        
