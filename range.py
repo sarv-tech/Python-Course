82970b9d4cc39a8724745ef803d9b5dae3084ca9
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2025-12-14 16:07:13 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,16 @@
+#  Write a Python program that prints numbers from 1 to 10 using a for loop and the range function.
+
+for i in range(1, 11):
+    print(i)  # This code prints numbers from 1 to 10 using a for loop and the range function.
+
+print("\n")
+
+# WAP to print numbers from 0 to 4 using range function.
+for i in range(0,5):
+    print(i)
+
+print("\n")
+
+# WAP to print numbers from 0 to 8 with a step of 3 using range function.
+for n in range(0,9,3):
+    print(n)
