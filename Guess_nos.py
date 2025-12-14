8cc2b11a9a5848731f8d76a7856e72cef18ba446
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2025-12-15 00:40:05 +0530

    Add files via upload

---
+++ Guess_nos.py
@@ -0,0 +1,21 @@
+# Q. Let’s create a Number Guessing Game.
+# Given  saecret number (already decided by you), write a program that asks the user to guess the number and prints:
+
+# If the guess is above the secret number → "Too high"
+# If the guess is below the secret number → "Too low"
+# If the guess matches the secret number → "Correct! You guessed it"
+
+
+secret_number = 32
+guess_number = float(input("Enter Guess Number: "))
+
+if(guess_number > secret_number):
+    print("Too high")
+elif(guess_number < secret_number):
+    print("Too Low")
+elif(guess_number == secret_number):
+    print("Correct! You guessed it")
+else:
+    print("Guess Number is Invalid!")
+
+print(guess_number)
