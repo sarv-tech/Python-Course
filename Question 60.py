c2bfc25a2e86a50304f30333e8a7498caf0c6f28
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2025-12-16 01:46:11 +0530

    Add files via upload

---
+++ Question 60.py
@@ -0,0 +1,20 @@
+# Q. Given a tuple of integers, create:
+# • A tuple of all even numbers
+# • A tuple of all odd numbers
+
+
+tup = (1, 2, 3, 4, 5, 6, 7, 8)
+even_list = []
+odd_list =  []
+
+for num in tup:
+    if num % 2 == 0:
+        even_list.append(num)
+    else:
+        odd_list.append(num)
+
+even_tup = tuple(even_list)
+odd_tup = tuple(odd_list)
+
+print("Even tuple is: ", even_tup)
+print("odd tuple is: ", odd_tup)
