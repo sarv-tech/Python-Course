79f2c48a95900a2994d47227b75f599f8c0b52ac
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2025-12-16 02:33:34 +0530

    Add files via upload

---
+++ Question 64.py
@@ -0,0 +1,9 @@
+# Q. Write a program that takes as string from the user and print the number of spaces in the string
+
+
+s = input("Enter a string: ")
+
+# Count spaces
+space_count = s.count(" ")
+
+print("Number of spaces in the string:",space_count)
