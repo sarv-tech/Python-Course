9160f667c747da5982e28d90cd128f8032bd8874
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2025-12-19 18:06:14 +0530

    Add files via upload

---
+++ OOP_pillars.py
@@ -0,0 +1,21 @@
+There are 4 pillars of OOPs :-
+
+# 1. Encapsulation => wrapping up of data and it's relative properties and methods into a single unit.
+
+# 2. Abstraction => Hiding unnecessary details from the user and showing only the necessary details.
+
+# 3. Inheritance => Jaha par hum ek class ki properties ko uski subclass ya children classes ke andar transfer karte hai.
+# When we transfer the properties of one class to its subclass or child classes, it is called Inheritance.
+
+# 4. Polymorphism => jiske andar same functions ya same operators ko hum form change karke in multiple ways use karte hai.
+# When we use the same functions or the same operators in different forms and in multiple ways, it is called Polymorphism.
+
+Access Modifiers :-
+Attributes
+
+# 1. Public => accessible in inside the class as well as outside the class by every function everywhere in the code. 
+
+# 2. Protected => accessible in inside the class + subsclass (Inheritance) by every function
+
+
+# 3. Private => accessible only in inside the class not outside the class by every function.
