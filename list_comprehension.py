0c1e23c906619a2028115befd34b0222e288b9d7
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-02 21:25:44 +0530

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -17,4 +17,38 @@ print("\n")
 
 # Square of 0 to 4
 squared = [x**2 for x in range(5)]
+
 print(squared)  
+
+print("\n")
+
+# Linear Search
+nums = [1,2,3,10,4]
+x = 10
+idx = 0
+
+for val in nums:
+    if(val == x):
+        print(f"{x} is found at index {idx}")
+        break
+    else:
+        idx += 1
+        
+print("\n")
+
+square = [i*i for i in range(6) if i % 2 != 0]
+print(square)
+
+print("\n")
+
+nums = [-2, -3, 3, 4, -1, 7]
+
+nums = [0 if val < 0 else val for val in nums]
+print(nums)
+
+print("\n")
+
+words = ["python", "hello", "sarvesh"]
+
+words = [val.upper() for val in words]
+print(words)
