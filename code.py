e35fab648654a54be030fae9d16c4d551a5a88e5
Sarvesh Pingale <pingalesarvesh5@gmail.com>
2026-01-03 00:39:36 +0530

    Add files via upload

--- code.py
+++ code.py
@@ -1,13 +1,29 @@
-data = True
-line = 1
-word = "Python"
+# Converting Python to JSON
+# We use dumps() to convert Python objects into JSON string.
+import json
 
-with open("sample.txt", "r") as f:
-    while data:
-        data = f.readline()
+data = { 
+    "name": "John",
+    "age": 25,
+    "marks": [85, 90, 92]
+}
 
-        if(word in data):
-            print(f"{word} found at line {line}")
-            break
+json_string = json.dumps(data) 
+print(json_string)
 
-        line += 1
+print("\n")
+
+# Converting JSON to python 
+# We use loads() to convert JSON string to Python dictionary
+json_data = '{"name": "John", "age": 25}' 
+python_obj = json.loads(json_data) 
+print(python_obj["name"])
+
+print("\n")
+
+# Writing JSON to a File (json.dump)
+import json
+
+data = {"name": "Aisha", "city": "Delhi"} 
+with open("data.json", "w") as f: 
+    json.dump(data, f, indent=4)
